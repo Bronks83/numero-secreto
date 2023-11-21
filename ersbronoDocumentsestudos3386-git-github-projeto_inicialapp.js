e6fe752cde5d1e183c255863d515c201325d7621
Bronkss <bronovox1@hotmail.com>
2023-11-20 21:12:46 -0300

    ainda mais fácil

---
+++ ersbronoDocumentsestudos3386-git-github-projeto_inicialapp.js
@@ -0,0 +1,31 @@
+[33mcommit c1150a5c64c5d8c3c4343189cf5e80c0fb0aa586[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m)[m
+Author: Bronkss <bronovox1@hotmail.com>
+Date:   Mon Nov 20 20:44:31 2023 -0300
+
+    Revert "deixando mais fácil"
+    
+    This reverts commit 7b1e968fb6cce9cca13286ce59693eb876e43ba0.
+
+[33mcommit 7b1e968fb6cce9cca13286ce59693eb876e43ba0[m
+Author: Bronkss <bronovox1@hotmail.com>
+Date:   Mon Nov 20 20:39:28 2023 -0300
+
+    deixando mais fácil
+
+[33mcommit 61d0180fc1170ce9adcd514cef8fd5542b1e21e1[m
+Author: Bronkss <bronovox1@hotmail.com>
+Date:   Mon Nov 20 20:26:43 2023 -0300
+
+    Deixando o jogo mais fácil
+
+[33mcommit 2b38ab3c976ca342eaa84486f37cded4167ef148[m
+Author: Bronkss <bronovox1@hotmail.com>
+Date:   Mon Nov 20 19:34:47 2023 -0300
+
+    alterando limite para 100
+
+[33mcommit 7151f1c69012b01189385e73ac506d2cd0c115be[m
+Author: Bronkss <bronovox1@hotmail.com>
+Date:   Mon Nov 20 17:32:39 2023 -0300
+
+    projeto inicial
